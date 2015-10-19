206fb62472ddc2d35846def6facfc0527fe3115c
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-19 20:47:10 +0200

    added View interface

--- src/main/java/es/upm/miw/voting/web/presentation/views/View.java
+++ src/main/java/es/upm/miw/voting/web/presentation/views/View.java
@@ -1,6 +1,6 @@
 package es.upm.miw.voting.web.presentation.views;
 
-import es.upm.miw.voting.presentation.models.Model;
+import es.upm.miw.voting.web.presentation.models.Model;
 
 public interface View {
     void show(Model model);
