eef23f831cdc5b9a7b7fc26b3d9cccef69490b41
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-20 20:01:21 +0200

    Added DaoFactoryMemory class

---
+++ src/main/java/es/upm/miw/voting/rest/data/models/daos/memory/DaoFactoryMemory.java
@@ -0,0 +1,32 @@
+package es.upm.miw.voting.rest.data.models.daos.memory;
+
+import es.upm.miw.voting.rest.data.models.daos.DaoFactory;
+import es.upm.miw.voting.rest.data.models.daos.ThemeDao;
+import es.upm.miw.voting.rest.data.models.daos.VoteDao;
+
+public class DaoFactoryMemory extends DaoFactory {
+
+    private ThemeDao themeDao;
+
+    private VoteDao voteDao;
+
+    public DaoFactoryMemory() {
+    }
+
+    @Override
+    public ThemeDao getThemeDao() {
+        if (themeDao == null) {
+            themeDao = new ThemeMemoryDao();
+        }
+        return themeDao;
+    }
+
+    @Override
+    public VoteDao getVoteDao() {
+        if (voteDao == null) {
+            voteDao = new VoteMemoryDao();
+        }
+        return voteDao;
+    }
+
+}
