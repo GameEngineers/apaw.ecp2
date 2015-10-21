a5a290d4daff74765b845d126e08afae8e71d175
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-21 18:31:56 +0200

    Added ThemesRequest class

---
+++ src/main/java/es/upm/miw/voting/rest/business/views/ThemesRequest.java
@@ -0,0 +1,15 @@
+package es.upm.miw.voting.rest.business.views;
+
+public class ThemesRequest {
+
+    String Theme;
+
+    public String getTheme() {
+        return Theme;
+    }
+
+    public void setTheme(String theme) {
+        Theme = theme;
+    }
+
+}
