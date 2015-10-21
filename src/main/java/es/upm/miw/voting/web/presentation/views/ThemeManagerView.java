f980ed7d6718267551197b322b028a3ee4d84a92
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-21 22:22:25 +0200

    Refactored ThemeView to ThemeManagerView

--- src/main/java/es/upm/miw/voting/web/presentation/views/ThemeManagerView.java
+++ src/main/java/es/upm/miw/voting/web/presentation/views/ThemeManagerView.java
@@ -2,7 +2,7 @@ package es.upm.miw.voting.web.presentation.views;
 
 import es.upm.miw.voting.web.presentation.models.Model;
 
-public class ThemeView implements View {
+public class ThemeManagerView implements View {
 
     @Override
     public void show(Model model) {
