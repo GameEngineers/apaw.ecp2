6af9bacdd36047b605941b8ce37b9e0d86bf0394
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-22 21:29:25 +0200

    added VotingPresenter class

---
+++ src/main/java/es/up/miw/voting/web/presentation/presenters/VotingPresenter.java
@@ -0,0 +1,23 @@
+package es.up.miw.voting.web.presentation.presenters;
+
+import es.upm.miw.voting.rest.business.controllers.VotingThemesBO;
+import es.upm.miw.voting.rest.business.views.VotingThemeDTO;
+import es.upm.miw.voting.web.presentation.models.Model;
+
+public class VotingPresenter {
+
+    public String process(Model model){
+        model.put("action", "VotingPresenter:process");
+        return "ThemeManagerView";
+    }
+    
+    public String voteTheme(Model model){
+        model.put("action", "VotingPresenter:voteTheme");
+        VotingThemeDTO dto = new VotingThemeDTO();
+        dto.setTheme(model.get("themeName").toString());
+        dto.setTheme(model.get("value").toString());
+        new VotingThemesBO().addVote(dto);
+        model.put("votingThemes", new VotingThemesBO().getVotingThemes());
+        return "VotingView";
+    }
+}
