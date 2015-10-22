5d04514794d80ebf5ebb6adb5fcc52477435e3bb
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-22 23:47:46 +0200

    updated ThemeManagerPresenter

--- src/main/java/es/up/miw/voting/web/presentation/presenters/ThemeManagerPresenter.java
+++ src/main/java/es/up/miw/voting/web/presentation/presenters/ThemeManagerPresenter.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import es.upm.miw.voting.rest.business.controllers.VotingThemesBO;
 import es.upm.miw.voting.rest.business.views.ThemeDto;
-import es.upm.miw.voting.rest.data.models.daos.DaoFactory;
 import es.upm.miw.voting.web.presentation.models.Model;
 
 public class ThemeManagerPresenter {
