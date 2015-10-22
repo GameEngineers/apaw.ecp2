0dab535dcf9634e5f5e24430a2266ae4c253cd56
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-22 19:36:34 +0200

    updated VotingThemeDTO

--- src/main/java/es/upm/miw/voting/rest/business/views/VotingThemeDTO.java
+++ src/main/java/es/upm/miw/voting/rest/business/views/VotingThemeDTO.java
@@ -2,9 +2,19 @@ package es.upm.miw.voting.rest.business.views;
 
 public class VotingThemeDTO {
 
+    int themeId;
+
     String theme;
 
     int vote;
+
+    public int getThemeId() {
+        return themeId;
+    }
+
+    public void setThemeId(int themeId) {
+        this.themeId = themeId;
+    }
 
     public String getTheme() {
         return theme;
