9bd35cb6435b6ef2f78b41fdda7c0fb5b4d46302
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-22 20:32:27 +0200

    Added VotingTheme class

---
+++ src/main/java/es/upm/miw/voting/web/presentation/views/VotingView.java
@@ -0,0 +1,13 @@
+package es.upm.miw.voting.web.presentation.views;
+
+import es.upm.miw.voting.web.presentation.models.Model;
+
+public class VotingView implements View {
+
+    @Override
+    public void show(Model model) {
+        System.out.println("Voting Page");
+        System.out.println("Temas: " + model.get("votingThemes"));        
+    }
+
+}
