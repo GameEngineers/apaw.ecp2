a506761aedcf7c34cf583f68aee21b57f907f6aa
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-22 23:21:40 +0200

    updated VotingThemeDto

--- src/main/java/es/upm/miw/voting/rest/business/views/VotingThemeDto.java
+++ src/main/java/es/upm/miw/voting/rest/business/views/VotingThemeDto.java
@@ -2,38 +2,44 @@ package es.upm.miw.voting.rest.business.views;
 
 public class VotingThemeDto {
 
-    int themeId;
-
     String theme;
 
+    double averege;
+    
     int vote;
-
-    public int getThemeId() {
-        return themeId;
-    }
-
-    public void setThemeId(int themeId) {
-        this.themeId = themeId;
-    }
 
     public String getTheme() {
         return theme;
     }
 
+
     public void setTheme(String theme) {
         this.theme = theme;
     }
+
+
+    public double getAverege() {
+        return averege;
+    }
+
+
+    public void setAverege(double averege) {
+        this.averege = averege;
+    }
+
 
     public int getVote() {
         return vote;
     }
 
+
     public void setVote(int vote) {
         this.vote = vote;
     }
 
+
     public String toString() {
-        return "ThemeTransfer [themeName=" + this.getTheme() + ", " + "average=" + this.getVote() + "]";
+        return "ThemeTransfer [themeName=" + this.getTheme() + ", " + "average=" + String.format("%1$,.2f", this.getAverege()) + "]";
     }
 
 }
