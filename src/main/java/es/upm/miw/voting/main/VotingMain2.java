6426930908ecfe8b1337df3f0ac7f3e7c913b556
Jorge Garrido <torkanwarrior@gmail.com>
2015-10-22 23:48:02 +0200

    added VoteMain2

---
+++ src/main/java/es/upm/miw/voting/main/VotingMain2.java
@@ -0,0 +1,72 @@
+package es.upm.miw.voting.main;
+
+import es.upm.miw.voting.rest.data.models.daos.DaoFactory;
+import es.upm.miw.voting.rest.data.models.daos.memory.DaoFactoryMemory;
+import es.upm.miw.voting.web.http.HttpMethod;
+import es.upm.miw.voting.web.http.HttpRequest;
+
+public class VotingMain2 {
+
+    public VotingMain2() {
+    }
+
+    public void voting(){
+        HttpRequest request = new HttpRequest("LocalHost", "Voting", HttpMethod.GET);
+        System.out.println(request.toString() + "\n");
+        
+        new Client().send(request);
+    }
+    
+    public void votingVoteTheme(String themeName, int value) {
+        HttpRequest request = new HttpRequest("LocalHost", "Voting", HttpMethod.POST);
+        request.addQueryParam("action", "voteTheme");
+        request.addQueryParam("themeName", themeName);
+        request.addQueryParam("value", "" + value);
+        System.out.println(request.toString() + "\n");
+        
+        new Client().send(request);
+    }
+
+    public void themeManager(){
+        HttpRequest request = new HttpRequest("LocalHost", "ThemeManager", HttpMethod.GET);
+        System.out.println(request.toString() + "\n");
+        
+        new Client().send(request);
+    }
+    
+    public void themeManagerCreateTheme(String themeName){
+        HttpRequest request = new HttpRequest("LocalHost", "ThemeManager", HttpMethod.POST);
+        request.addQueryParam("action", "createTheme");
+        request.addQueryParam("themeName", themeName);
+        System.out.println(request.toString() + "\n");
+        
+        new Client().send(request);
+    }
+    
+
+    public static void main(String[] args) {
+        DaoFactory.setFactory(new DaoFactoryMemory());
+        VotingMain2 main = new VotingMain2();
+        System.out.println("-------Voting--------------------------------------------------------");
+        main.voting();
+        System.out.println("-------ThemeManager--------------------------------------------------");
+        main.themeManager();
+        System.out.println("---");
+        main.themeManagerCreateTheme("Tema1");
+        System.out.println("---");
+        main.themeManagerCreateTheme("Tema2");
+        System.out.println("-------Voting--------------------------------------------------------");
+        main.votingVoteTheme("Tema1", 4);
+        System.out.println("---");
+        main.votingVoteTheme("Tema1", 6);
+        System.out.println("---");
+        main.votingVoteTheme("Tema1", 5);
+        System.out.println("---");
+        main.votingVoteTheme("Tema2", 5);
+        System.out.println("---");
+        main.votingVoteTheme("Tema1", 3);
+        System.out.println("-------ThemeManager--------------------------------------------------");
+        main.themeManager();
+    }
+
+}
